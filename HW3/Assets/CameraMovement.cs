e71f429f3ff5d53b0e1c55c74a2cb0125fa77cf2
Adam Austin <adam.marcus.austion@gmail.com>
2019-10-27 15:02:39 -0600

    H3

---
+++ HW3/Assets/CameraMovement.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public GameObject parent;
+	private int targetLayer = 1 << 8; // Layer 8 (targets)
+	private static bool fireIsDown = false;
+	private static Vector3 lastRaycastHit;
+	public CapsulePrefab teleLocation;
+	public CapsulePrefab tempTele;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetAxis("Horizontal") != 0)
+		{
+			// Move the attached parent based on the right vector of this object multiplied by the horizontal axis value
+			parent.transform.position += (transform.right) * Input.GetAxis("Horizontal") / 4;
+		}
+
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			// Move the attached parent based on the right vector of this object multiplied by the horizontal axis value
+			parent.transform.position += (transform.forward) * Input.GetAxis("Vertical") / 4;
+		}
+
+
+
+        // User pressed the button, raycast to create the indicator and set button press down.
+		if (Input.GetAxis("Fire1") == 1 && !fireIsDown)
+		{
+			// The Unity raycast hit object, which will store the output of our raycast
+
+			RaycastHit hit;
+			//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			// Does the ray intersect any objects excluding the player layer
+			// Parameters: position to start the ray, direction to project the ray, output for raycast, limit of ray length, and layer mask
+			if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, targetLayer))
+			{
+				// The object we hit will be in the collider property of our hit object.
+				// We can get the name of that object by accessing it's Game Object then the name property
+				Debug.Log(hit.collider.gameObject.name);
+				
+				Debug.Log("Initial Hit");
+				Debug.Log(hit.point.x + ", " + hit.point.y);
+
+				tempTele = Instantiate(teleLocation);
+				updateLastRaycastHit(hit);
+				tempTele.transform.position = lastRaycastHit;
+				fireIsDown = true;
+			}
+			else
+			{
+				Debug.Log("No initial Hit");
+			}
+		}
+
+
+
+        // User is holding the button down, move the indicator
+		if (Input.GetAxis("Fire1") == 1 && fireIsDown)
+		{
+			Debug.Log("Held Down Fire");
+
+			// The Unity raycast hit object, which will store the output of our raycast
+
+			RaycastHit hit;
+			//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			// Does the ray intersect any objects excluding the player layer
+			// Parameters: position to start the ray, direction to project the ray, output for raycast, limit of ray length, and layer mask
+			if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, targetLayer))
+			{
+				// The object we hit will be in the collider property of our hit object.
+				// We can get the name of that object by accessing it's Game Object then the name property
+				Debug.Log(hit.collider.gameObject.name);
+
+				Debug.Log("Moved hit");
+				Debug.Log(hit.point.x + ", " + hit.point.y);
+
+                updateLastRaycastHit(hit);				
+
+				tempTele.transform.position = lastRaycastHit;
+			}
+			else
+			{
+				Debug.Log("No moved Hit");
+			}
+
+		}
+
+
+
+        // User has released the button, teleport and destroy indicator
+		if (Input.GetAxis("Fire1") == 0 && fireIsDown)
+		{
+			Debug.Log("Released Fire Button");
+			parent.transform.position = lastRaycastHit;
+			Debug.Log("Teleported");
+			Debug.Log(lastRaycastHit);
+
+			Destroy(tempTele.gameObject);
+
+			fireIsDown = false;
+		}
+	}
+
+    void updateLastRaycastHit(RaycastHit hit)
+	{
+		lastRaycastHit = new Vector3(hit.point.x, hit.point.y + 1.0f, hit.point.z);
+	}
+}
---
+++ HW3/Assets/LogMonitor.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMonitor : MonoBehaviour
+{
+	// Will be used to store a reference to the text mesh component
+	private TextMesh textMesh;
+
+	// Use this for initialization
+	void Start()
+	{
+		// Get the instance of the TextMesh component on this game object and store it
+		textMesh = gameObject.GetComponentInChildren<TextMesh>();
+	}
+
+    void Update()
+	{
+		//ResetLogMessage();
+		//Debug.Log("Hello Debug");
+		//Joystick left = 0 -> -1
+		//Joystick right = 0 -> 1
+		if (Input.GetAxis("Horizontal") != 0)
+			Debug.Log("Horizontal: " + Input.GetAxis("Horizontal"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Vertical") != 0)
+			Debug.Log("Vertical: " + Input.GetAxis("Vertical"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Fire1") != 0)
+			Debug.Log("Fire1: " + Input.GetButtonDown("Fire1"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Fire2") != 0)
+			Debug.Log("Fire2: " + Input.GetAxis("Fire2"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Fire3") != 0)
+			Debug.Log("Fire3: " + Input.GetAxis("Fire3"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Jump") != 0)
+			Debug.Log("Jump: " + Input.GetButtonDown("Jump"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Mouse X") != 0)
+			Debug.Log("Mouse X: " + Input.GetAxis("Mouse X"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Mouse Y") != 0)
+			Debug.Log("Mouse Y: " + Input.GetAxis("Mouse Y"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Mouse ScrollWheel") != 0)
+			Debug.Log("Mouse ScrollWheel: " + Input.GetAxis("Mouse ScrollWheel"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1if (!= 0)
+		if (Input.GetAxis("Submit") != 0)
+			Debug.Log("Submit: " + Input.GetAxis("Submit"));
+		//Joystick up = 0 -> 1
+		//Joystick down = 0 -> -1
+		if (Input.GetAxis("Cancel") != 0)
+			Debug.Log("Cancel: " + Input.GetAxis("Cancel"));
+
+		//string temp = Input.inputString;
+		//if (temp != " ")
+		//	//ResetLogMessage();
+		//	Debug.Log(temp);
+
+		//Trigger key = 1
+	}
+
+	//Called by Unity when this object is enabled in the scene
+	void OnEnable()
+	{
+		// Attach the LogMessage function as a callback for the logMessageReceived event in Unity
+		Application.logMessageReceived += LogMessage;
+	}
+
+	// Called by Unity when this object is disabled in the scene
+	void OnDisable()
+	{
+		Application.logMessageReceived -= LogMessage;
+	}
+
+	public void LogMessage(string message, string stackTrace, LogType type)
+	{
+		// Set the text 
+		//textMesh.text = textMesh.text + "\n" + message;
+		textMesh.text = message;
+	}
+
+	public void ResetLogMessage()
+	{
+		// Set the text 
+		textMesh.text = "";
+	}
+}
